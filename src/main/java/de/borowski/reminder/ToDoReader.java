df25d866c0d509008055882d1caf31d680630817
CBorowski-dev <christoph.borowski@gmx.de>
2025-07-11 12:56:17 +0200

    Added JSON parsing

--- src/main/java/de/borowski/reminder/ToDoReader.java
+++ src/main/java/de/borowski/reminder/ToDoReader.java
@@ -1,15 +1,40 @@
 package de.borowski.reminder;
 
+import jakarta.json.*;
 import org.springframework.batch.item.ItemReader;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ToDoReader implements ItemReader<String> {
-    private String[] data = {"Spring", "Batch", "Example"};
     private int index = 0;
+    private JsonArray jsonArray;
+
+    public ToDoReader() {
+        super();
+        JsonReader reader = null;
+        try {
+            reader = Json.createReader(
+                    new FileReader("/home/christoph/Projects/IdeaProjects/Reminder/src/main/resources/todos.json"));
+            // Parse the JSON string into a JsonObject
+            jsonArray = reader.readArray();
+        } catch (FileNotFoundException e) {
+            // do nothing
+        }
+    }
 
     @Override
     public String read() throws Exception {
-        if (index < data.length) {
-            return data[index++]; // Returns the next string in the array
+        if (jsonArray != null && index < jsonArray.size()) {
+            JsonObject todo =  jsonArray.getJsonObject(index++);
+            StringBuffer sb = new StringBuffer();
+            sb.append(String.format("%s : %s --> %s\n", todo.getString("todo"), todo.getString("description"), todo.getString("mailAddress")));
+            JsonArray dates = todo.getJsonArray("dates");
+            for (JsonValue jv : dates) {
+                sb.append(jv.toString());
+                sb.append('\n');
+            }
+            return sb.toString();
         } else {
             return null;  // Signals the end of reading
         }
